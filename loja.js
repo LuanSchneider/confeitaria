fd0db93f1619e94864c5d5cfb248c0269c6b9719
LuanSchneider <143182587+LuanSchneider@users.noreply.github.com>
2023-10-09 12:09:48 -0300

    Atualização

--- loja.js
+++ loja.js
@@ -71,3 +71,40 @@ function atualizarTotal() {
 // Add a change event to the quantity selector
 quantidade.addEventListener("change", atualizarTotal);
 
+function finalizar2() {
+  // Seleciono o elemento button do HTML
+  var button = document.querySelector("button");
+  // Faço alguma coisa com o botão, por exemplo, redirecionar para outra página
+  button.addEventListener("click", function() {
+    window.location.href = "https://www.bing.com/";
+  });
+}
+var quantidade = document.getElementById("quantidade-select");
+var quantidade = document.getElementById("quantidade-select2");
+
+
+
+
+// Select the td element that contains the total of the product
+var total = quantidade.parentElement.parentElement.lastElementChild.previousElementSibling;
+
+// Create a function to update the total of the product
+function atualizarTotal() {
+// Get the value of the quantity
+var qtd = parseInt(quantidade.value);
+
+// Get the unit price of the product
+var preco = parseInt(quantidade.parentElement.previousElementSibling.textContent.replace('R$ ', ''));
+
+// Calculate the total of the product
+var totalProduto = qtd * preco;
+
+
+// Update the cell of the total of the product
+total.textContent = "R$ " + totalProduto;
+}
+
+
+// Add a change event to the quantity selector
+quantidade.addEventListener("change", atualizarTotal);
+
